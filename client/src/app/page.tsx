fb3ddd80bb1dc99c0f45d5310610dff28557ec4a
Radmir Gurkov <ragux0@gmail.com>
2025-05-23 22:55:35 +0200

    Init rooms

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,26 +1,43 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+
+const userId = uuid();
 
 export default function Home() {
+  const [client, setClient] = useState<WebSocket | null>(null);
   const [message, setMessage] = useState<string>("");
   useEffect(() => {
     const client = new WebSocket("ws://localhost:8080");
     client.onopen = () => {
       console.log("WebSocket connection opened");
-      client.send("Hello, server!");
     };
     client.onmessage = (event) => {
       setMessage(JSON.stringify(event.data));
-      console.log("Message from server:", event.data);
     };
     client.onerror = (error) => {
       console.error("WebSocket error:", error);
     };
+    setClient(client);
     return () => {
       client.close();
       console.log("WebSocket connection closed");
     };
   }, []);
-  return <div className="grid">{message}</div>;
+
+  if (!client) {
+    return <div>Connecting to socket</div>;
+  }
+
+  const createRoom = () => {
+    client.send(JSON.stringify({ type: "create", roomId: userId }));
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-24">
+      <p>{`Your room id: ${userId}`}</p>
+      <p>{message}</p>
+      <button onClick={createRoom}>Create room</button>
+    </div>
+  );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,61 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 console.log("WebSocket server started on ws://localhost:8080");
+
+interface Client {
+  ws: WebSocket;
+  id: string;
+  roomId?: string;
+}
+
+interface Room {
+  id: string;
+  clients: Set<Client>;
+}
+
+const rooms = new Map<string, Room>();
+
+type MessageType = "join" | "leave" | "message" | "joined" | "left" | "create";
+
+type Message = {
+  type: MessageType;
+  [key: string]: any;
+};
+
+function createRoom(roomId: string): Room {
+  const room: Room = { id: roomId, clients: new Set() };
+  rooms.set(roomId, room);
+  return room;
+}
+
+function joinRoom(client: Client, roomId: string): void {
+  let room = rooms.get(roomId);
+  if (!room) {
+    throw new Error(`Room ${roomId} does not exist`);
+  }
+  room.clients.add(client);
+  client.roomId = roomId;
+}
+
+function leaveRoom(client: Client): void {
+  if (!client.roomId) {
+    throw new Error("Client is not in a room");
+  }
+  const room = rooms.get(client.roomId!);
+  if (room) {
+    room.clients.delete(client);
+    if (room.clients.size === 0) {
+      rooms.delete(client.roomId!);
+    }
+  }
+}
+
+function sendMessageToRoom(room: Room, message: Message): void {
+  room.clients.forEach((client) => {
+    client.ws.send(JSON.stringify(message));
+  });
+}
 
 wss.on("connection", (ws) => {
   ws.on("error", (error) => {
@@ -9,7 +63,24 @@ wss.on("connection", (ws) => {
   });
 
   ws.on("message", (message) => {
-    console.log("Received:", message);
-    ws.send(`Echo: ${message}`);
+    try {
+      const msg = JSON.parse(message.toString()) as Message;
+      switch (msg.type) {
+        case "create": {
+          const room = createRoom(msg.roomId);
+          const client: Client = { ws, id: msg.id, roomId: room.id };
+          room.clients.add(client);
+          sendMessageToRoom(room, {
+            type: "joined",
+            message: "User joined room",
+          });
+          break;
+        }
+        default:
+          break;
+      }
+    } catch (error) {
+      console.error("Error parsing message:", error);
+    }
   });
 });
